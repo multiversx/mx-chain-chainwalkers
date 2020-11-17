cabaa48c49fe21a66cfe4fc6bf6168f4c8d2588f
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-11-17 17:17:13 +0200

    Compute tx fee (best effort). Rely on erdpy for some functionality (e.g. is_contract_address).

--- v2/walkpy/main.py
+++ v2/walkpy/main.py
@@ -1,9 +1,13 @@
+import base64
 import logging
 import sys
 from argparse import ArgumentParser, FileType
 from typing import Any
 
-from walkpy import constants, errors, utils
+from erdpy import utils
+from erdpy.accounts import Address
+
+from walkpy import constants, errors
 from walkpy.api import ElrondGateway
 
 
@@ -35,7 +39,7 @@ def get_height(args: Any):
     outfile = args.outfile
 
     height = gateway.get_chain_height(constants.METASHARD_ID)
-    utils.dump_json({"height": height}, outfile)
+    utils.dump_out_json({"height": height}, outfile)
 
 
 def parse_blocks(args: Any):
@@ -53,12 +57,41 @@ def parse_blocks(args: Any):
             hyperblock = {"nonce": nonce}
         blocks.append(hyperblock)
 
-    utils.dump_json(blocks, outfile)
+    utils.dump_out_json(blocks, outfile)
 
 
 def post_process_hyperblock(block: dict):
+    # The timestamp can be computed deterministically, given the round and the genesis time
     round = int(block.get("round", 0))
     block["timestamp"] = constants.ERD_START_TIME + constants.ERD_ROUND_TIME * round
+
+    # Hyperblocks V1 does not provide the transaction fees (nor "gasUsed"). Hyperblocks V2 will provide this information.
+    # Until then, we can compute the fees deterministically (best-effort).
+    # For Smart Contract transactions, the refunds will be incompletely provided by the API - until Hyperblocks V2 becomes available.
+    # (e.g. intra-shard refunds are not visible etc.)
+
+    for transaction in block["transactions"]:
+        tx_type = transaction.get("type")
+        tx_gas_limit = int(transaction.get("gasLimit", 0))
+        tx_gas_price = int(transaction.get("gasPrice", 0))
+        tx_receiver = Address(transaction.get("receiver"))
+        tx_data = transaction.get("data", None)
+        tx_data_decoded = ""
+        tx_data_len = 0
+        tx_gas_used = 0
+
+        if tx_data:
+            tx_data_decoded = base64.b64decode(tx_data)
+            tx_data_len = len(tx_data_decoded)
+
+        if tx_type == "normal":
+            if tx_receiver.is_contract_address():
+                tx_gas_used = tx_gas_limit
+            else:
+                tx_gas_used = constants.ERD_MIN_GAS_LIMIT + tx_data_len * constants.ERD_GAS_PER_DATA_BYTE
+
+        fee = tx_gas_used * tx_gas_price
+        transaction["fee"] = str(fee)
 
 
 if __name__ == "__main__":
